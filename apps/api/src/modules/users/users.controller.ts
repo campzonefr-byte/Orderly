0119c3be962efc119d8c4e96e4d9bbdcacffd451
hamdimarabet <hamdi.marabet@gmail.com>
2026-09-05 05:51:18 +0200

    make accept-invite endpoint public

--- apps/api/src/modules/users/users.controller.ts
+++ apps/api/src/modules/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Patch, Delete, Body, Param, UseGuards, Request } from '@nestjs/common';
+
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { Controller, Get, Post, Patch, Delete, Body, Param, UseGuards, Request, SetMetadata } from '@nestjs/common';
